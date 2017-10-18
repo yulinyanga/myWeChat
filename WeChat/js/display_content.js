7c13ecc1f1ec4e1e79fe26978abb8406e9ba178b
yulinyanga <1163363675@qq.com>
2017-10-18 11:18:47 +0800

    发消息

--- WeChat/js/display_content.js
+++ WeChat/js/display_content.js
@@ -1,7 +1,8 @@
-function display_index() {
-	$(".nav_btn_no").css({
-		"outline": "none"
-	});
+$(function() {
+	$("#voice_key").hide();
+	$("#chatExtra").hide();
+	$("#chat-bottom").hide();
+	$("#chat-content").hide();
 	$("#content-index").show();
 	$("#content-discover").hide();
 	$("#content-ad_book").hide();
@@ -11,20 +12,37 @@ function display_index() {
 	$("#tool").show();
 	$("#tool_camera").hide();
 	$("#tool_discover").hide();
+	$("#tool_people").hide();
+	$("#tool_index").hide();
+	$("#tool_add_ex").hide();
+	$(".nav_btn_no").css({
+		"outline": "none"
+	});
+	$("#file").hide();
+	$("#weixin").attr('color', "rgb(102,187,106)");
+});
+
+function display_index() {
+
+	$("#chat-bottom").hide();
+	$("#content-index").show();
+	$("#content-discover").hide();
+	$("#content-ad_book").hide();
+	$("#content-me").hide();
+	$("#headline").text("微信(1)");
+	$("#tool").show();
 	$("#pic1").attr('src', "img/icon/menu/聊天-press.svg");
 	$("#pic2").attr('src', "img/icon/menu/通讯录.svg");
 	$("#pic3").attr('src', "img/icon/menu/发现.svg");
 	$("#pic4").attr('src', "img/icon/menu/me.svg");
-	document.getElementById("weixin").style.color="rgb(102,187,106)";
-	document.getElementById("txl").style.color="rgb(158,158,158)";
-	document.getElementById("fx").style.color="rgb(158,158,158)";
-	document.getElementById("wo").style.color="rgb(158,158,158)";
+	$("#weixin").attr('color', "rgb(102,187,106)");
+	$("#txl").attr('color', "rgb(158,158,158)");
+	$("#fx").attr('color', "rgb(158,158,158)");
+	$("#wo").attr('color', "rgb(158,158,158)");
 }
 
 function display_ad_book() {
-	$("#friend_btn").css({
-		"outline": "none"
-	});
+
 	$("#content-index").hide();
 	$("#content-ad_book").show();
 	$("#content-discover").hide();
@@ -35,17 +53,14 @@ function display_ad_book() {
 	$("#pic2").attr('src', "img/icon/menu/通讯录-press.svg");
 	$("#pic3").attr('src', "img/icon/menu/发现.svg");
 	$("#pic4").attr('src', "img/icon/menu/me.svg");
-	document.getElementById("weixin").style.color="rgb(158,158,158)";
-	document.getElementById("txl").style.color="rgb(102,187,106)";
-	document.getElementById("fx").style.color="rgb(158,158,158)";
-	document.getElementById("wo").style.color="rgb(158,158,158)";
+	$("#weixin").attr('color', "rgb(158,158,158)");
+	$("#txl").attr('color', "rgb(102,187,106)");
+	$("#fx").attr('color', "rgb(158,158,158)");
+	$("#wo").attr('color', "rgb(158,158,158)");
 }
 
 function display_discover() {
-	$(".nav_btn_no").css({
-		"outline": "none"
-	});
-	
+
 	$("#content-index").hide();
 	$("#content-ad_book").hide();
 	$("#content-discover").show();
@@ -56,17 +71,13 @@ function display_discover() {
 	$("#pic2").attr('src', "img/icon/menu/通讯录.svg");
 	$("#pic3").attr('src', "img/icon/menu/发现-press.svg");
 	$("#pic4").attr('src', "img/icon/menu/me.svg");
-	document.getElementById("weixin").style.color="rgb(158,158,158)";
-	document.getElementById("txl").style.color="rgb(158,158,158)";
-	document.getElementById("fx").style.color="rgb(102,187,106)";
-	document.getElementById("wo").style.color="rgb(158,158,158)";
+	$("#weixin").attr('color', "rgb(158,158,158)");
+	$("#txl").attr('color', "rgb(158,158,158)");
+	$("#fx").attr('color', "rgb(102,187,106)");
+	$("#wo").attr('color', "rgb(158,158,158)");
 }
 
 function display_me() {
-	$(".nav_btn_no").css({
-		"outline": "none"
-	});
-	document.getElementById("wo").style.color="rgb(102,187,106)";
 	$("#content-index").hide();
 	$("#content-ad_book").hide();
 	$("#content-discover").hide();
@@ -77,27 +88,21 @@ function display_me() {
 	$("#pic2").attr('src', "img/icon/menu/通讯录.svg");
 	$("#pic3").attr('src', "img/icon/menu/发现.svg");
 	$("#pic4").attr('src', "img/icon/menu/me-press.svg");
-	document.getElementById("weixin").style.color="rgb(158,158,158)";
-	document.getElementById("txl").style.color="rgb(158,158,158)";
-	document.getElementById("fx").style.color="rgb(158,158,158)";
-	document.getElementById("wo").style.color="rgb(102,187,106)";
+	$("#weixin").attr('color', "rgb(158,158,158)");
+	$("#txl").attr('color', "rgb(158,158,158)");
+	$("#fx").attr('color', "rgb(158,158,158)");
+	$("#wo").attr('color', "rgb(102,187,106)");
 }
-//$(".praise_area").hide();
+
 function display_friends() {
-	$(".nav_btn_no").css({
-		"outline": "none"
-	});
-	$("#friends").show();
+	$("#friends").show(300);
 	$("#content-discover").hide();
 	$("#tool_camera").show();
-	$("#tool").hide();
-	$("#content-index").hide();
-	$("#content-ad_book").hide();
-	$("#content-me").hide();
 	$("#headline").text("朋友圈");
 	$("#tool_discover").show();
 	$("#nav").hide();
 }
+
 var add_no = true;
 
 function display_tool_add() {
@@ -110,52 +115,12 @@ function display_tool_add() {
 	}
 
 }
- 
 
-var parise_no1 = true;
-
-function display_praise1(){
-	
-	if(parise_no1) {
-		$("#praise_btn1").show();
-		parise_no1 = false;
-	} else {
-		$("#praise_btn1").hide();
-		parise_no1 = true;
-	}
-}
-var parise_no2 = true;
-
-function display_praise2(){
-	
-	if(parise_no2) {
-		$("#praise_btn2").show();
-		parise_no2 = false;
-	} else {
-		$("#praise_btn2").hide();
-		parise_no2 = true;
-	}
-}
-var parise_no3 = true;
-
-function display_praise3(){
-	
-	if(parise_no3) {
-		$("#praise_btn3").show();
-		parise_no3 = false;
-	} else {
-		$("#praise_btn3").hide();
-		parise_no3 = true;
-	}
-}
-
-function back_discover(){
-	$(".nav_btn_no").css({
-		"outline": "none"
-	});
+function back_discover() {
 	$("#friends").hide();
 	$("#content-discover").show();
 	$("#tool_camera").hide();
 	$("#tool_discover").hide();
 	$("#nav").show();
+	$("#headline").text("发现");
 }
